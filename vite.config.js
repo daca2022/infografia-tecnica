46ff87a75f9c495216730fe9e5a7554d4bb7fd3e
daca2022 <david.caballero@utec.edu.pe>
2025-06-13 20:23:59 -0500

    Configurar despliegue automático con GitHub Actions

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'src/index.html'),
+    },
+  },
+  base: '/infografia-tecnica/', // Ajusta esto si tu repo tiene otro nombre
+});
